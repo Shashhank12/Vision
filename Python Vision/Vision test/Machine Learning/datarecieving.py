83e273e9fb93b4092d3127f835b52417c4644230
Shashhank12 <59421940+Shashhank12@users.noreply.github.com>
2022-02-16 17:57:04 -0800

    added positive

--- Python Vision/Vision test/Machine Learning/datarecieving.py
+++ Python Vision/Vision test/Machine Learning/datarecieving.py
@@ -6,19 +6,24 @@ from time import time
 
 
 #Captures video
-wincap = VideoCapture(0)
+wincap = VideoCapture('Machine Learning\WIN_20220215_17_21_51_Pro.mp4')
 #Sets loop-time to time
 loop_time = time()
 #For running each frame
+lower_green = np.array([220, 220, 220])
+upper_green = np.array([255, 255, 255])
 while(True):
     isTrue, frame = wincap.read()
     
     cv.imshow('Unprocessed', frame)
+    # gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    mask = cv.inRange(frame, lower_green, upper_green)
     #Prints FPS
     print('FPS {}'. format(1/(time() - loop_time)))
     loop_time = time()
     #Sets key to waitkey
-    key = cv.waitKey(1)
+    cv.imwrite("Machine Learning\Positive\{}.jpg".format(loop_time), mask)
+    key = cv.waitKey(10)
     #When q is pressed, window closes
     if key == ord('q'):
         cv.destroyAllWindows
